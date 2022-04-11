36427eab78ead9cb3a0389d40767f0c82f73eaf6
minvenanne <ariise21@student.aau.dk>
2022-04-11 11:40:18 +0200

    test

---
+++ app/src/main/java/com/example/kei_data/Settings_activity1.java
@@ -0,0 +1,14 @@
+package com.example.kei_data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Settings_activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings1);
+    }
+}
