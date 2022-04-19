eb56dbcd02271b82f2316200b1b073bdc6f41872
Casper Anisimow <81159246+anisthepooh@users.noreply.github.com>
2022-04-19 12:36:43 +0200

    Category

--- app/src/main/java/com/example/kei_data/MainActivity.java
+++ app/src/main/java/com/example/kei_data/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     public ImageButton settingsButton;
     public ImageButton categoriesbutton;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
