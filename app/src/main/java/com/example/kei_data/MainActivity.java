46f80cadda11ba27aa0f1847942e6d9ece46e716
minvenanne <ariise21@student.aau.dk>
2022-04-22 13:07:00 +0200

    22/04/2022 13.06

--- app/src/main/java/com/example/kei_data/MainActivity.java
+++ app/src/main/java/com/example/kei_data/MainActivity.java
@@ -11,6 +11,14 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.ImageButton;
 
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.util.Log;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.CompoundButton;
+
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,11 +26,42 @@ public class MainActivity extends AppCompatActivity {
     public ImageButton settingsButton;
     public ImageButton categoriesButton;
 
+    private RadioGroup radioGroupPhoto;
+    private ImageView imageViewPhoto;
+    private int []photos = {R.drawable.d, R.drawable.graph2, R.drawable.m, R.drawable.six_m,R.drawable.y};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.imageViewPhoto = (ImageView) findViewById(R.id.imageView);
+        this.radioGroupPhoto = (RadioGroup) findViewById(R.id.radioGroup);
+        this.radioGroupPhoto.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton radioButton = (RadioButton) radioGroup.findViewById(i);
+                int index = radioGroup.indexOfChild(radioButton);
+                imageViewPhoto.setImageResource(photos[index]);
+            }
+        });
+
+        Switch householdSwitch = findViewById(R.id.household);
+//            Lets the user switch between two modes.
+        householdSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (householdSwitch.isChecked()) {
+                    Log.d("Success", "Household");
+
+                } else {
+                    Log.d("Success", "you");
+                }
+            }
+        });
+        //END// SWITCH BETWEEN YOU AND HOUSEHOLD
 
         settingsButton = (ImageButton) findViewById(R.id.Settings);
         settingsButton.setOnClickListener(new View.OnClickListener() {
