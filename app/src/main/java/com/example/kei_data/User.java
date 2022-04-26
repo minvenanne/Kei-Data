d1a2d8ce6ea35c3f90dda8d5d4d53a81cb3f747a
engulcitron <92179600+engulcitron@users.noreply.github.com>
2022-04-26 10:29:17 +0200

    3 udkast: alle attributes skulle nu være sat rigtigt op

--- app/src/main/java/com/example/kei_data/User.java
+++ app/src/main/java/com/example/kei_data/User.java
@@ -1,6 +1,9 @@
 package com.example.kei_data;
 
+
 import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User {
 
@@ -8,61 +11,70 @@ public class User {
 
     public String userID;
 
-    //public String nfcID;
-
-    public Integer deviceDisplayID;
-
     public Date dateAdded;
-
-    //public Boolean userAdded;
-
-    //public Boolean userRemoved;
 
     ArrayList<Device> deviceList;
 
     ArrayList<DataUse> dataUseList;
 
-    public User (String n) {
-        userName = n;
-        setUserID("u0");
-        //setNfcID("n0");
-        setDeviceDisplayID(0);
-        //setUserAdded(true);
+    public User () {
+        setUserName("n");
+        setUserID();
         setDateAdded(); // få styr på denne
-        //setUserRemoved(false);
 
         this.deviceList = new ArrayList<Device>();
         this.dataUseList = new ArrayList<DataUse>();
     }
 
-    public void setUserID(String newUserID) {
-        userID = newUserID;
-        //laves til tilfældig værdi som ikke allerede eksisterer
+    public void setUserName(String newUserName) {
+        userName = newUserName;
     }
 
+    public void setUserID() {
+
+        //length of arraylist users + 1
+        userID = userList.size() + 1;
+    }
+
+    public void setDateAdded() {
+        dateAdded = new Date();
+        //dato aflæses og sættes som værdi her
+    }
+
+    //attributes:
+    //public Boolean userAdded;
+
+    //public Boolean userRemoved;
+
+    //public String nfcID;
+
+    //public Integer deviceDisplayID;
+
+    //add to "object":
+    //setNfcID("n0");
+    //setDeviceDisplayID(0);
+    //setUserAdded(true);
+    //setUserRemoved(false);
+
+    //Methods:
     //public void setNfcID(String newNfcID) {
         //nfcID = newNfcID;
         //laves til tilfældig værdi som ikke allerede eksisterer
     //}
 
-    public void setDeviceDisplayID(Integer newDeviceDisplayID) {
-        deviceDisplayID = newDeviceDisplayID;
+    //public void setDeviceDisplayID(Integer newDeviceDisplayID) {
+        //deviceDisplayID = newDeviceDisplayID;
         //potentielt set tager den her knap indputtet(eventlistener og sætter lig et device (if x eventlistener then x)
-    }
+    //}
 
-    public void setUserAdded(Boolean newUserAddedStatus) {
-        userAdded = newUserAddedStatus;
+    //public void setUserAdded(Boolean newUserAddedStatus) {
+        //userAdded = newUserAddedStatus;
         //laves til tilfældig værdi som ikke allerede eksisterer
-    }
+    //}
 
-    public void setDateAdded(Date newDateAdded) {
-        dateAdded = newDateAdded;
+    //public void setUserRemoved(Boolean newUserRemovedStatus) {
+        //userRemoved = newUserRemovedStatus;
         //laves til tilfældig værdi som ikke allerede eksisterer
-    }
-
-    public void setUserRemoved(Boolean newUserRemovedStatus) {
-        userRemoved = newUserRemovedStatus;
-        //laves til tilfældig værdi som ikke allerede eksisterer
-    }
+    //}
 
 }
