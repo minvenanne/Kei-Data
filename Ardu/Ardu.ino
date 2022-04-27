81ddbb8737af8e48206fb71d4d4ae2699f61cb0f
MagDamPa <92024310+MagDamPa@users.noreply.github.com>
2022-04-27 10:32:48 +0200

    Update Ardu.ino
    Yderligere kommentarer til den eksistrende kode,for at  forbedre læsbarheden

--- Ardu/Ardu.ino
+++ Ardu/Ardu.ino
@@ -3,11 +3,22 @@
 #include <SdFat.h>                // SD card & FAT filesystem library
 #include <Adafruit_SPIFlash.h>    // SPI / QSPI flash library
 #include <Adafruit_ImageReader.h> // Image-reading functions
-
+#include <SoftwareSerial.h> //Bluetooth module
+//Softwareserial stammer fra bluetoothmodulet. Vi deklarerer, at Blue modtager og transmiterer på hhvs. digital pin 2 og 3.
+SoftwareSerial Blue(2, 3);
+// Her deklarerer vi global variables, som vi 
+long int data;
+long int password1 = 92;// light on, eller i vores tilfælde, inkrementer billede med 1.
+long int password2 = 79; // light off
+int counter = 1; //Counter er lig nummeret på billedet der skal displayes; Hver eneste gang bluetooth modtager kommunikation, inkrementeres counter med 1. Når counter 4, reduceres counter til 1.
+boolean newData = false; //Evaluerer om data er modtaget; Hvis ja, skal counter inkrementeres med 1.
 
 //Liste over billeder på SD-kortet (Det er formateret i MS-DOS Fat):
-// adabot240.bmp
-// dedede.bmp
+// image1.bmp
+// image2.bmp
+// image3.bmp
+// image4.bmp
+// Husk at billeder skal være i opløsningen 240*280.
 // Comment out the next line to load from SPI/QSPI flash instead of SD card:
 #define USE_SD_CARD
 #define SD_CS    4 // SD card select pin
@@ -40,11 +51,11 @@ Adafruit_Image       img;        // An image loaded into RAM
 int32_t              width  = 0, // BMP image dimensions
                      height = 0;
 
-int counter = 1;
-boolean newData = false;
 void setup(void) {
 
-Serial.begin(9600);
+Serial.begin(9600); //Begynder kommunikation på serial port, med en baud-hastighed på 9600. Ikke nødvendigt, men kun til at hjælpe med at debugge bluetooth
+Blue.begin(9600); //Gør det samme for bluetooth
+
 #if !defined(ESP32)
   while(!Serial);       // Wait for Serial Monitor before continuing
 #endif
@@ -81,6 +92,18 @@ Serial.begin(9600);
 }
 
 void loop() {
+
+  //while(Blue.available()==0) ; Gør intet, før bluetooth er connected.
+
+ if(Blue.available()>0)  //Hvis bluetooth er tilgængelig, send da en værdi til Serial;
+{
+
+Serial.println("1234");
+  
+data = Blue.parseInt(); // Modtag den transmiterede bluetooth værdi, og opbevar den i data
+  } 
+Serial.println(data); // Udskriv den modtagne værdi til serial - ikke nødvendigt, men praktisk til debugging. 
+
   Updatecounter();
   showdata();
   printing();
@@ -88,14 +111,24 @@ void loop() {
 }
 
 void Updatecounter(){
-if (Serial.available() > 0) {
-    char newInput =  Serial.read();
+if (data == password1) //Hvis den modtagne værdi er korrekt, sæt newdata til true
+   {
+//    char newInput =  Serial.read();
     newData = true;
   }
 }
+
+void showdata(){
+  if (newData == true){ //Hvis newdata er true, inkrementer da counter med 1, og sæt newdata til false
+    counter = counter + 1;
+    newData = false;
+    data=0; //Sæt data til 0, dermed "cleares" den senest modtagne bluetooth-værdi
+  }  
+}
+
 void printing(){
 ImageReturnCode stat; // Status from image-reading functions
-  
+  ///Tegn det billede der svarer til counter
   while (counter == 1){
      // Serial.println(F("Loading image1.bmp to screen..."));
   stat = reader.drawBMP("/image1.bmp", tft, 0, 0);
@@ -122,15 +155,10 @@ while (counter == 4) {
 }
 
 }
-void showdata(){
-  if (newData == true){
-    counter = counter + 1;
-    newData = false;
-  }  
-}
 
 void checking(){
   if (counter > 4) {
     counter = 1;
+    //Sætter counter til 1, i tilfælde af at counter er over 5, eller, returner til det første billede.
   }  
 }
