d6a07340df38002167d2d6053f87f82e43f54fb5
engulcitron <92179600+engulcitron@users.noreply.github.com>
2022-04-29 14:25:04 +0200

    1: updates lavet på userclass og remmer sat op for device - mangler at blive koblet til activity og "setDeviceIP" som måske ikke kan laves endnu?

---
+++ app/src/main/java/com/example/kei_data/Device.java
@@ -0,0 +1,83 @@
+package com.example.kei_data;
+
+import static com.example.kei_data.User.setNumberOfDevices;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+//setting up the variables
+public class Device {
+
+    public String deviceType;
+
+    public Integer deviceID;
+
+    public String deviceIP;
+
+    public Date dateAdded;
+
+    public String deviceName;
+
+    public Boolean deviceAdded;
+
+    public Boolean deviceRemoved;
+
+    ArrayList<DataUse> dataUseList;
+
+    //constructor creating a n0 user
+    public Device () {
+        setDeviceName("n"); //skal kobles op med activity så den tager indput derfra
+        setDeviceID();
+        setDateAdded();
+        setDeviceIP(); // mangler alt - kan måske først sættes op når der er lavet en falsk database af IP ala den til datause som anne har lavet.
+        setDeviceAdded(true);
+        setDeviceRemoved(false);
+        setDeviceType("type");
+
+        // creates list of class datause
+        this.dataUseList = new ArrayList<DataUse>();
+    }
+
+    //set device name to content of parenthesis
+    public void setDeviceName(String name) {
+        deviceName = name;
+    }
+
+    // set the date added to date of creation
+    public void setDateAdded() {
+        dateAdded = new Date();
+    }
+
+    //add a device to the list of devices
+    public void addDataUse(DataUse dataUse){
+        dataUseList.add(dataUse);
+    }
+    public void setDeviceID() {
+        //length of arraylist device
+        setNumberOfDevices();
+
+        //device ID is set to number of devices in the list
+        deviceID = User.numberOfDevices;
+    }
+
+    //set status of added to true or false
+    public void setDeviceAdded (Boolean addedStatus) {
+        deviceAdded = addedStatus;
+    }
+
+    //set status of removed to true or false
+    public void setDeviceRemoved (Boolean removedStatus) {
+        deviceRemoved = removedStatus;
+    }
+
+    //set device type to fx tv or computer
+    public void setDeviceType(String type) {
+        deviceType = type;
+    }
+
+    // should help users locate IP
+    public void setDeviceIP() {
+    //indhold her
+    }
+
+}
